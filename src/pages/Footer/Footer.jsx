2973c7fda27a2c98d07c9c474a4982d70f17b086
Blitstarz <Bitstarz@packmen.agency>
2025-08-12 19:51:10 +0300

    l

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -16,31 +16,32 @@ const Footer = () => {
                 <div className="footer-social">
                     <span className="footer-social-title">{t('footer.findUsOn')}</span>
                     <div className="footer-social-icons">
-                        <a href="https://facebook.com/yourpage" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.facebook.com/mBank.Polska" target="_blank" rel="noopener noreferrer">
                             <span className="icon-circle">
                                 <img src="/social/facebook.svg" alt="Facebook"/>
                             </span>
                         </a>
-                        <a href="https://instagram.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com/mbankpl/" target="_blank" rel="noopener noreferrer">
                             <span className="icon-circle">
                                 <img src="/social/insta.svg" alt="Instagram"/>
                             </span>
                         </a>
-                        <a href="https://x.com/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <a href="https://x.com/mBankpl" target="_blank" rel="noopener noreferrer">
                             <span className="icon-circle">
                                 <img src="/social/x.svg" alt="X"/>
                             </span>
                         </a>
-                        <a href="https://linkedin.com/company/yourcompany" target="_blank" rel="noopener noreferrer">
-                            <span className="icon-circle">
-                                <img src="/social/linkedin.svg" alt="LinkedIn"/>
-                            </span>
-                        </a>
-                        <a href="https://youtube.com/yourchannel" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.youtube.com/mBankTV" target="_blank" rel="noopener noreferrer">
                             <span className="icon-circle">
                                 <img src="/social/youtube.svg" alt="YouTube"/>
                             </span>
                         </a>
+                        <a href="https://www.linkedin.com/company/mbank-s-a" target="_blank" rel="noopener noreferrer">
+                            <span className="icon-circle">
+                                <img src="/social/linkedin.svg" alt="LinkedIn"/>
+                            </span>
+                        </a>
+
                     </div>
                     <div className="footer-languages">
                         <span className="footer-languages-title"></span>
@@ -50,13 +51,13 @@ const Footer = () => {
 
                 <div className="footer-mobile-apps">
                     <div className="footer-mobile-title">{t('footer.mobileApplication')}</div>
-                    <a href="https://play.google.com/store" target="_blank" rel="noopener noreferrer">
+                    <a href="https://play.google.com/store/apps/details?id=pl.mbank" target="_blank" rel="noopener noreferrer">
                         <img src="/stores/googleplay.svg" alt="Google Play" />
                     </a>
-                    <a href="https://apps.apple.com/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://itunes.apple.com/pl/app/mbank-pl/id454987093?mt=8" target="_blank" rel="noopener noreferrer">
                         <img src="/stores/appstore.svg" alt="App Store" />
                     </a>
-                    <a href="https://appgallery.huawei.com/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://appgallery.cloud.huawei.com/ag/n/app/C102250427?channelId=EUPLBDD20201120MB&detailType=0" target="_blank" rel="noopener noreferrer">
                         <img src="/stores/appgallery.svg" alt="AppGallery" />
                     </a>
                 </div>
